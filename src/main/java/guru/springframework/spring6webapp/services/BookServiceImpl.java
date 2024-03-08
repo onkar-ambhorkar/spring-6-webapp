05a721c89bd48790fc424f308b722ea8862b29db
onkar-ambhorkar <onkar.ambhorkar@gmail.com>
2024-03-08 13:34:30 +0530

    book service added

---
+++ src/main/java/guru/springframework/spring6webapp/services/BookServiceImpl.java
@@ -0,0 +1,18 @@
+package guru.springframework.spring6webapp.services;
+
+import guru.springframework.spring6webapp.model.Book;
+import guru.springframework.spring6webapp.repositories.BookRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BookServiceImpl implements BookService {
+
+    private final BookRepository bookRepository;
+
+    @Override
+    public Iterable<Book> findAll() {
+        return bookRepository.findAll();
+    }
+}
