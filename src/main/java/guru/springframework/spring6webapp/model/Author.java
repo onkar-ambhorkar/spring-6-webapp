7c5f32615493f053ea754a282e6decf83e7c6724
onkar-ambhorkar <onkar.ambhorkar@gmail.com>
2024-03-08 09:19:43 +0530

    equal and hashcode annotation added

--- src/main/java/guru/springframework/spring6webapp/model/Author.java
+++ src/main/java/guru/springframework/spring6webapp/model/Author.java
@@ -3,12 +3,14 @@ package guru.springframework.spring6webapp.model;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.util.Set;
 
 @Entity
 @Data
 @Builder
+@EqualsAndHashCode(of = "id")
 public class Author {
 
     @Id
